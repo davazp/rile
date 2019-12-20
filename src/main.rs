fb3d6432899ad1908dd2d6cdee256f1bdf291302
David Vázquez Púa <davazp@gmail.com>
2019-12-20 15:25:41 +0100

    Read characters in a loop until 'q' is pressed

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@ use nix::libc::STDIN_FILENO;
 use nix::unistd;
 
 fn main() {
-    let mut buf = [0];
+    let mut buf = [0u8];
 
-    let result = unistd::read(STDIN_FILENO, &mut buf).unwrap();
-    println!("read {} bytes: {:?}", result, buf);
+    loop {
+        let result = unistd::read(STDIN_FILENO, &mut buf).unwrap();
+
+        match buf[0] as char {
+            'q' => break,
+            _ => (),
+        };
+
+        println!("read {} bytes: {:?}", result, buf);
+    }
 }
